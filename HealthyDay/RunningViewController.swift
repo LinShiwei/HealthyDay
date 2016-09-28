feebecb60febb6cf1dde8b278d6527b192945d7e
LinShiwei <477623614@qq.com>
2016-09-28 13:17:40 +0800

    [Coding] Add RunningVC & mapView

---
+++ HealthyDay/RunningViewController.swift
@@ -0,0 +1,37 @@
+//
+//  RunningViewController.swift
+//  HealthyDay
+//
+//  Created by Linsw on 16/9/28.
+//  Copyright © 2016年 Linsw. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class RunningViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
