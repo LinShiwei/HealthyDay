5fe89794e69eda9c7367b046afe3dd26d2a7ce66
Lin Shiwei <linshw@foxmail.com>
2016-09-29 14:12:00 +0800

    [Coding] Add trace user's running (#1)

--- HealthyDay/RunningViewController.swift
+++ HealthyDay/RunningViewController.swift
@@ -12,56 +12,73 @@ import MapKit
 class RunningViewController: UIViewController {
 
     let locationManager = LocationManager()
-
+    var runningCoordiantes = [CLLocationCoordinate2D]()
+    
+    var hasLocated = false
+    var startRunning = false
+    
     @IBOutlet weak var descriptionLabel: UILabel!
     @IBOutlet weak var mapView: MKMapView!
     @IBAction func tapToStartRunning(_ sender: UIButton) {
+        startRunning = !startRunning
+        runningCoordiantes.removeAll()
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        mapView.delegate = self
         locationManager.authorize()
-
-        // Do any additional setup after loading the view.
+        mapView.delegate = self
         mapView.showsUserLocation = true
-        print(mapView.userLocation)
-        print(mapView.userLocation.location)
-        let lat = mapView.userLocation.location?.coordinate.latitude
-        let lon = mapView.userLocation.location?.coordinate.longitude
-        descriptionLabel.text = "\(lat) : \(lon)"
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
+    
+    fileprivate func drawRoute(withCoordiantes coordinates:[CLLocationCoordinate2D]){
+        guard coordinates.count > 1 else{return}
+        let overlay = MKPolyline(coordinates: coordinates, count: coordinates.count)
+        mapView.removeOverlays(mapView.overlays)
+        mapView.add(overlay)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        print(mapView.overlays.count)
     }
-    */
-
 }
 
 extension RunningViewController: MKMapViewDelegate{
     func mapViewWillStartLocatingUser(_ mapView: MKMapView) {
         
     }
+    
     func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
         guard let location = userLocation.location else{return}
+        if !mapView.isUserLocationVisible {
+            mapView.setCenter(location.coordinate, animated: true)
+        }
+        
         let lat = location.coordinate.latitude
         let lon = location.coordinate.longitude
         descriptionLabel.text = "\(lat) : \(lon)"
         
-        let userRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 1000.0, 1000.0)
-        mapView.region = userRegion
-        
+        if !hasLocated {
+            let userRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 1000.0, 1000.0)
+            mapView.setRegion(userRegion, animated: true)
+            hasLocated = true
+        }else{
+            if startRunning {
+                drawRoute(withCoordiantes: runningCoordiantes)
+                runningCoordiantes.append(location.coordinate)
+            }else{
+                
+            }
+        }
     }
+    
+    func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let render = MKPolylineRenderer(overlay: overlay)
+        render.fillColor = UIColor.blue
+        render.strokeColor = UIColor.red
+        render.lineWidth = 5.0
+        
+        return render
+    }
+    
 }
