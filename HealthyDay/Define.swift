5f0f380c3af90d8112fb7fb6bd2781dd2fa4748e
Lin Shiwei <linshw@foxmail.com>
2016-10-04 21:46:22 +0800

    Decorate mainInfoView (#5)
    * [Coding] Decorate mainInfoView
    
    * [Coding] refactor

--- HealthyDay/Define.swift
+++ HealthyDay/Define.swift
@@ -9,3 +9,19 @@
 import Foundation
 import UIKit
 let windowBounds = UIScreen.main.bounds
+
+struct ThemeColor {
+
+    let lightColor: UIColor
+    let thickColor: UIColor
+}
+
+let theme = ThemeColor(
+    lightColor:rgbColor(red:147, green:249, blue: 185, alpha: 1),
+    thickColor:rgbColor(red: 29, green: 151, blue: 108, alpha: 1)
+)
+
+
+func rgbColor(red:CGFloat, green:CGFloat, blue:CGFloat, alpha:CGFloat)->UIColor{
+    return UIColor(red: red/255.0, green: green/255.0, blue: blue/255.0 , alpha: alpha)
+}
--- HealthyDay/MainInformationView.swift
+++ HealthyDay/MainInformationView.swift
@@ -14,10 +14,14 @@ enum MainInfoViewState{
 }
 
 class MainInformationView: UIView{
-    private let dot = UIView()
+//MARK: Property
+    
+    private let containerView = UIView()
     private let stepCountLabel = UILabel()
     private let distanceWalkingRunningLabel = UILabel()
-    private let containerView = UIView()
+    private let dot = UIView()
+    private let labelMaskView = UIView()
+    private let decorativeView = UIView()
     
     private var state = MainInfoViewState.distance
     
@@ -61,15 +65,26 @@ class MainInformationView: UIView{
             distanceWalkingRunningLabel.text = "\(distance) Meters"
         }
     }
-    
+//MARK: View
     override init(frame:CGRect){
         super.init(frame:frame)
-        backgroundColor = UIColor.yellow
+        
+        
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [theme.lightColor.cgColor,theme.thickColor.cgColor]
+        layer.addSublayer(gradientLayer)
+        
         initContainerView(rotateRadius: frame.width/2+frame.height/4)
-//        initContainerView(withSuperView: self, rotateRadius: frame.height/4)
         initDotView()
+        initLabelMaskView()
+        initButtomDecorativeView()
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+//MARK: Custom View
     private func initContainerView(rotateRadius radius:CGFloat){
         containerView.center = CGPoint(x: frame.width/2, y: frame.height/2+radius)
         
@@ -95,10 +110,36 @@ class MainInformationView: UIView{
     
     private func initDotView(){
         dot.frame.size = CGSize(width: 10, height: 10)
-        dot.center = CGPoint(x: frame.width*2/5, y: 10)
+        dot.center = CGPoint(x: frame.width*2/5, y: frame.height/4 - 10)
         dot.layer.cornerRadius = dot.frame.width
-        dot.layer.backgroundColor = UIColor.black.cgColor
+        dot.layer.backgroundColor = UIColor.white.cgColor
         addSubview(dot)
+    }
+
+    private func initLabelMaskView(){
+        labelMaskView.frame = CGRect(x: 0, y: frame.height, width: frame.width, height: windowBounds.height-frame.height)
+        labelMaskView.backgroundColor = UIColor.white
+        addSubview(labelMaskView)
+    }
+    
+    private func initButtomDecorativeView(){
+        let height = frame.height/10
+        let width = frame.width
+        decorativeView.frame = CGRect(x: 0, y: frame.height-height, width: width, height: height)
+        let path = UIBezierPath()
+        path.move(to: CGPoint(x: 0, y: height))
+        path.addCurve(to: CGPoint(x: width, y:height), controlPoint1: CGPoint(x:width/3,y:0), controlPoint2: CGPoint(x:width/3*2,y:0))
+        path.addLine(to: CGPoint(x: 0, y: height))
+        path.close()
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        shapeLayer.lineWidth = 1
+        shapeLayer.strokeColor = UIColor.white.cgColor
+        shapeLayer.fillColor = UIColor.white.cgColor
+        
+        decorativeView.layer.addSublayer(shapeLayer)
+        addSubview(decorativeView)
     }
     
     func swipeClockwise(){
@@ -112,7 +153,5 @@ class MainInformationView: UIView{
         dot.center.x = frame.width*3/5
         state = .step
     }
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
+   
 }
