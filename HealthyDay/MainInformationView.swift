4e50d1da8baa3c058ef87f214c9926f9696acb93
LinShiwei <477623614@qq.com>
2016-10-04 14:10:20 +0800

    [Coding] Add a dot in mainInfoView

--- HealthyDay/MainInformationView.swift
+++ HealthyDay/MainInformationView.swift
@@ -9,6 +9,7 @@
 import UIKit
 
 class MainInformationView: UIView {
+    private let dot = UIView()
     private let stepCountLabel = UILabel()
     private let distanceWalkingRunningLabel = UILabel()
     private let containerView = UIView()
@@ -25,16 +26,16 @@ class MainInformationView: UIView {
     
     override init(frame:CGRect){
         super.init(frame:frame)
-        backgroundColor = UIColor.red
-        initContainerView(withSuperView: self, rotateRadius: frame.width/2+frame.height/4)
+        backgroundColor = UIColor.yellow
+        initContainerView(rotateRadius: frame.width/2+frame.height/4)
 //        initContainerView(withSuperView: self, rotateRadius: frame.height/4)
-
+        initDotView()
     }
     
-    private func initContainerView(withSuperView superView:UIView, rotateRadius radius:CGFloat){
-        containerView.center = CGPoint(x: superView.frame.width/2, y: superView.frame.height/2+radius)
+    private func initContainerView(rotateRadius radius:CGFloat){
+        containerView.center = CGPoint(x: frame.width/2, y: frame.height/2+radius)
         
-        distanceWalkingRunningLabel.frame.size = CGSize(width: superView.frame.width/2, height: superView.frame.height/2)
+        distanceWalkingRunningLabel.frame.size = CGSize(width: frame.width/2, height: frame.height/2)
         distanceWalkingRunningLabel.center = CGPoint(x: 0, y: -radius)
         distanceWalkingRunningLabel.textAlignment = .center
         distanceWalkingRunningLabel.adjustsFontSizeToFitWidth = true
@@ -51,15 +52,25 @@ class MainInformationView: UIView {
         
         containerView.addSubview(stepCountLabel)
         containerView.addSubview(distanceWalkingRunningLabel)
-        superView.addSubview(containerView)
+        addSubview(containerView)
+    }
+    
+    private func initDotView(){
+        dot.frame.size = CGSize(width: 10, height: 10)
+        dot.center = CGPoint(x: frame.width*2/5, y: 10)
+        dot.layer.cornerRadius = dot.frame.width
+        dot.layer.backgroundColor = UIColor.black.cgColor
+        addSubview(dot)
     }
     
     func swipeClockwise(){
         containerView.transform = .identity
+        dot.center.x = frame.width*2/5
     }
     
     func swipeCounterclockwise(){
         containerView.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
+        dot.center.x = frame.width*3/5
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
