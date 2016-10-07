9283d410e2cae24164804ee7c895cd0248368094
Lin Shiwei <linshw@foxmail.com>
2016-10-07 15:12:05 +0800

    [Coding] Improve ringView animation (#6)

--- HealthyDay/StepRingView.swift
+++ HealthyDay/StepRingView.swift
@@ -12,15 +12,20 @@ class StepRingView: UIView,CAAnimationDelegate {
     
     private let dotCount : CGFloat = 60
     private let dotRadius : CGFloat = 1
+    private let ringGap : CGFloat = 3
     
-    private var dotContainerLayer : CAReplicatorLayer?
-    private let backgroundLayer = CAReplicatorLayer()
-    private let staticDotContainerLayer = CAReplicatorLayer()
+    let maskLayer = CAReplicatorLayer()
+    let strokeLayer = CAShapeLayer()
+    let ringLayer = CALayer()
     
     var precent : Double = 0{
         didSet{
-            staticDotContainerLayer.instanceCount = Int(Double(dotCount)*precent)
-            performRingAnimation()
+            precent = precent > 1 ? 1 : precent
+            let strokeEnd = CABasicAnimation(keyPath: "strokeEnd")
+            strokeEnd.fromValue = 1
+            strokeEnd.toValue = precent
+            strokeEnd.duration = (1-precent)*1
+            strokeLayer.ocb_applyAnimation(strokeEnd)
         }
     }
     
@@ -29,79 +34,62 @@ class StepRingView: UIView,CAAnimationDelegate {
         super.init(frame:frame)
         self.precent = precent
         
-        let staticDotLayer = CALayer()
-        staticDotLayer.frame = CGRect(x: -dotRadius, y: 0, width: dotRadius*2, height: dotRadius*2)
-        staticDotLayer.cornerRadius = dotRadius
-        staticDotLayer.backgroundColor = UIColor.white.cgColor
-        
-        staticDotContainerLayer.frame = bounds
-        staticDotContainerLayer.instanceCount = Int(Double(dotCount)*precent)
-        staticDotContainerLayer.instanceColor = UIColor.white.cgColor
-        staticDotContainerLayer.instanceTransform = CATransform3DMakeRotation(CGFloat.pi*2/dotCount, 0, 0, 1)
-        staticDotContainerLayer.transform = CATransform3DMakeRotation(CGFloat.pi/4, 0, 0, 1)
-        staticDotContainerLayer.addSublayer(staticDotLayer)
+        let dot = CALayer()
+        dot.frame = CGRect(x: -dotRadius, y: 0, width: 2*dotRadius, height: 2*dotRadius)
+        dot.cornerRadius = dotRadius
+        dot.backgroundColor = UIColor.white.cgColor
 
-        let backgroundDot = CALayer()
-        backgroundDot.frame = CGRect(x: -dotRadius, y: 0, width: 2*dotRadius, height: 2*dotRadius)
-        backgroundDot.cornerRadius = dotRadius
-        backgroundDot.backgroundColor = UIColor.white.cgColor
+        maskLayer.frame = CGRect(x: (size.width-size.width/1.41)/2, y: (size.width-size.width/1.41)/2, width: size.width/1.41, height: size.width/1.41)
+        maskLayer.instanceCount = Int(dotCount)
+        maskLayer.instanceColor = UIColor.darkGray.cgColor
+        maskLayer.instanceTransform = CATransform3DMakeRotation(CGFloat.pi*2/dotCount, 0, 0, 1)
+        maskLayer.transform = CATransform3DMakeRotation(CGFloat.pi/4, 0, 0, 1)
+        maskLayer.addSublayer(dot)
         
-        backgroundLayer.frame = bounds
-        backgroundLayer.instanceCount = Int(dotCount)
-        backgroundLayer.instanceColor = UIColor.darkGray.cgColor
-        backgroundLayer.instanceTransform = CATransform3DMakeRotation(CGFloat.pi*2/dotCount, 0, 0, 1)
-        backgroundLayer.transform = CATransform3DMakeRotation(CGFloat.pi/4, 0, 0, 1)
-        backgroundLayer.addSublayer(backgroundDot)
-
+        let circlePath : CGPath = {
+            let path = CGMutablePath()
+            path.addArc(center: CGPoint(x:size.width/2,y:size.height/2), radius: size.width/2, startAngle: -CGFloat.pi/2, endAngle: CGFloat.pi/2*3, clockwise: false)
+            return path
+        }()
+        
+        strokeLayer.frame = bounds
+        strokeLayer.path = circlePath
+        strokeLayer.fillColor = nil
+        strokeLayer.strokeColor = UIColor.white.cgColor
+        strokeLayer.lineWidth = dotRadius*3
+        strokeLayer.mask = maskLayer
+        strokeLayer.backgroundColor = UIColor.darkGray.cgColor
+        
+        strokeLayer.actions = [
+            "strokeEnd":NSNull()
+        ]
+        strokeLayer.strokeEnd = 0.3
+        
+        layer.addSublayer(strokeLayer)
+        
         let ringLayer = CALayer()
-        ringLayer.frame = CGRect(x: -size.width/2*0.41+3, y: -size.width/2*0.41+3, width: size.width*1.41-6, height: size.height*1.41-6)
+        ringLayer.frame = CGRect(x: ringGap, y: ringGap, width: bounds.width-ringGap*2, height: bounds.height-ringGap*2)
         ringLayer.borderColor = UIColor(white: 1, alpha: 0.2).cgColor
         ringLayer.borderWidth = 1
         ringLayer.cornerRadius = ringLayer.frame.width/2
-        
-        layer.addSublayer(backgroundLayer)
         layer.addSublayer(ringLayer)
-        layer.addSublayer(staticDotContainerLayer)
-        
-        performRingAnimation()
-
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
-        print("stop")
-        guard let dCLayer = dotContainerLayer else{return}
-        dCLayer.removeFromSuperlayer()
-        dotContainerLayer = nil
-    }
-    
-    func performRingAnimation(){
-        guard dotContainerLayer == nil else{return}
-        let duration : CGFloat = 1
-        let fadeAnimation = CABasicAnimation(keyPath: "opacity")
-        fadeAnimation.duration = CFTimeInterval(duration)
-        fadeAnimation.fromValue = 0
-        fadeAnimation.toValue = 0
-        fadeAnimation.delegate = self
-        
-        let dotLayer = CALayer()
-        dotLayer.frame = CGRect(x: -dotRadius, y: 0, width: 2*dotRadius, height: 2*dotRadius)
-        dotLayer.cornerRadius = dotRadius
-        dotLayer.backgroundColor = UIColor.white.cgColor
-        dotLayer.add(fadeAnimation, forKey: "Fade")
-        
-        dotContainerLayer = CAReplicatorLayer()
-        dotContainerLayer!.frame = bounds
-        dotContainerLayer!.instanceCount = Int(dotCount)
-        dotContainerLayer!.instanceDelay = CFTimeInterval(duration/dotCount)
-        dotContainerLayer!.instanceColor = UIColor.white.cgColor
-        dotContainerLayer!.instanceTransform = CATransform3DMakeRotation(-CGFloat.pi*2/dotCount, 0, 0, 1)
-        dotContainerLayer!.transform = CATransform3DMakeRotation(CGFloat.pi/4+CGFloat.pi*0/3, 0, 0, 1)
-        dotContainerLayer!.addSublayer(dotLayer)
-        layer.insertSublayer(dotContainerLayer!, above: backgroundLayer)
+}
 
+extension CALayer {
+    func ocb_applyAnimation(_ animation: CABasicAnimation) {
+        let copy = animation.copy() as! CABasicAnimation
+        
+        if copy.fromValue == nil {
+            copy.fromValue = self.presentation()!.value(forKeyPath: copy.keyPath!)
+        }
+        
+        self.add(copy, forKey: copy.keyPath)
+        //由于这个动画的fillMode是Backwards所以addAnimation后，直接进入动画的初始状态，使得下面的setValue在视觉上并没有立即生效。下面的setValue反应的是最终的效果值
+        self.setValue(copy.toValue, forKeyPath:copy.keyPath!)
     }
 }
